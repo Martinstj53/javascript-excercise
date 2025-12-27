e4d5ee8d7eb2d5abdb3744902135b391f6678fa9
Martinstj53 <sodunkemartins@gmail.com>
2025-12-27 10:22:43 +0100

    update Excercise1.js

--- Excercise1.js
+++ Excercise1.js
@@ -29,7 +29,8 @@ let finalprice = price - discount
 console.log(finalprice)
 
 // 5.
-if (number > 3)
+let number = -5;
+if (number > 0)
     console.log('the number is positive')
 else if (number < 0)
     console.log('the number is negative')
