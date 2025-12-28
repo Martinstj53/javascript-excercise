a4fdbfed7fc7ec3200ccb93e4e5d2e2b694b0723
Martinstj53 <sodunkemartins@gmail.com>
2025-12-29 00:11:00 +0100

    Create Excercise2.js

---
+++ Excercise2.js
@@ -0,0 +1,37 @@
+// 6.
+let score = 75;
+if (score >= 50)
+    console.log('pass')
+else
+    console.log('Fail')
+
+// 7.
+let a = 12;
+let b = 15;
+if (a > b)
+    console.log(a + ' ' + 'is greater than' + ' ' + b)
+else
+    console.log(a + ' ' + 'is not greater than' + ' ' + b)
+
+// 8.
+let age = 16;
+let legalage = 18;
+if (age >= legalage)
+    console.log('can vote')
+else
+    console.log('cannot vote')
+
+// 9.
+let isWeekend = true
+if (isWeekend)
+    console.log('No work today')
+else
+    console.log('Time to work!')
+
+//10.
+let num = 24;
+if (num % 2 === 0)
+    console.log(num + ' ' + 'is even')
+else
+    console.log(num + ' ' + 'is old')
+
